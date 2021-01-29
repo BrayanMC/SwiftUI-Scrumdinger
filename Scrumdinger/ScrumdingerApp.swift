1300268f3978ac1d796f297045f1e48a1443e63e
Bryan Muñoz Campos <bmunozc@proveedor.intercorp.com.pe>
2021-01-29 11:26:51 -0500

    upload project

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,26 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Brayan Munoz Campos on 27/01/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    @ObservedObject private var data = ScrumData()
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ScrumsView(scrums: $data.scrums) {
+                    data.save()
+                }
+            }
+            .onAppear {
+                data.load()
+            }
+        }
+    }
+}
